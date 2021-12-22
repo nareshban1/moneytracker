0523b0d8df6ab4d21d7f4bd9e5397f2fa6c42690
Naresh Ban <np03a170060@heraldcollege.edu.np>
2021-12-22 10:55:05 +0545

    service worker changes

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -7,7 +7,6 @@ const urlsToCache = [
     './favicon.ico',
     './default.png',
     './',
-    '/fonts.gstatic.com/',
     'https://fonts.gstatic.com/s/poppins/v15/pxiEyp8kv8JHgFVrJJfecg.woff2',
     'https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap',];
 
