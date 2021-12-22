2da55871a42790f87e423c6e46ec61cd511dd74f
Naresh Ban <np03a170060@heraldcollege.edu.np>
2021-12-22 10:49:13 +0545

    service worker changtes

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,7 +1,6 @@
 const CACHE_NAME = "budget"
-const urlsToCache = ['./static/js/bundle.js',
-    './static/js/vendors-main.chunk.js',
-    './static/js/main.chunk.js',
+const urlsToCache = [
+    '/static/js/main.ccafb8c8.js',
     './manifest.json',
     './logo512.png',
     './logo192.png',
@@ -9,6 +8,7 @@ const urlsToCache = ['./static/js/bundle.js',
     './default.png',
     './',
     '/fonts.gstatic.com/',
+    'https://fonts.gstatic.com/s/poppins/v15/pxiEyp8kv8JHgFVrJJfecg.woff2',
     'https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap',];
 
 const self = this;
@@ -24,10 +24,14 @@ self.addEventListener('fetch', (event) => {
     if (!navigator.onLine) {
         event.respondWith(
             caches.match(event.request).then((response) => {
-                return response || fetch(event.request).then((response) => {
-                    caches.put(event.request, response.clone());
+                if (response) {
                     return response
-                })
+                }
+                else {
+                    fetch(event.request)
+                }
+
+
 
             })
         )
